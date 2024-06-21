260397226dd77634f8a9dd5557e0e7261541cb7f
ZhuJiaqi <zhujiaqi9905@gmail.com>
2024-06-21 14:37:29 +0800

    modify scripts for v0, v1

--- exp/start_container.sh
+++ exp/start_container.sh
@@ -5,7 +5,10 @@ addrs=(172.31.11.113  172.31.11.170)
 image="oobleck:v12"
 
 # Define the common part of the docker run command
-docker_command="docker run --gpus device=%d -d -v ~/code/python/:/workspace --net=host --ipc=host --device=/dev/infiniband/rdma_cm --device=/dev/infiniband/uverbs0 --ulimit memlock=-1:-1 --name oob-%d ${image} sleep infinity"
+# docker_command="docker run --gpus device=%d -d -v ~/code/python/:/workspace --net=host --ipc=host --device=/dev/infiniband/rdma_cm --device=/dev/infiniband/uverbs0 --ulimit memlock=-1:-1 --name oob-%d ${image} sleep infinity"
+
+docker_command="docker run --gpus device=%d -d -v ~/code/python/:/workspace --net=host --ipc=host --ulimit memlock=-1:-1 --name oob-%d ${image} sleep infinity"
+
 
 for addr in "${addrs[@]}"; do
     # ssh ${addr} "$(printf "${docker_command}" 0 0)
