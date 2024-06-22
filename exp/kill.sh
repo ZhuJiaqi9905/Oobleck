5663721cac83bb7e8644c84828836d99705cdfac
Ubuntu <ubuntu@ip-172-31-10-88.ec2.internal>
2024-06-22 07:06:14 +0000

    add kill script

--- exp/kill.sh
+++ exp/kill.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+# run in container. only need to install on oob-0
+# addrs=("172.31.11.113" "172.31.9.213")
+# ports=(2220)
 
-pids=$(ps aux | grep 'oobleck.ela' | grep -v grep | awk '{print $2}')
+addrs=(172.31.10.88 172.31.8.235)
+ports=(2220 2221 2222 2223 2224 2225 2226 2227)
 
-# 检查是否有找到进程
-if [ -z "$pids" ]; then
-    echo "not found 'pretrain' process."
-else
-    # 循环终止每个PID对应的进程
-    for pid in $pids; do
-        echo "kill process PID: $pid"
-        kill -9 "$pid"
+
+for port in "${ports[@]}"; do
+    for addr in "${addrs[@]}"; do
+        echo "in ${addr}:${port}"
+        ssh -p ${port} ${addr} -o "StrictHostKeyChecking no" "cd /workspace/Oobleck && ./exp/local_kill.sh"
     done
-fi
+done
