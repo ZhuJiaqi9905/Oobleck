c0c41f6a3bb25c12aa94e8c1043969d015fe18be
Ubuntu <ubuntu@ip-172-31-35-195.ec2.internal>
2024-10-10 08:53:12 +0000

    add install config

--- install.sh
+++ install.sh
@@ -38,11 +38,11 @@ source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda create -n oobleck python==3.10.0
 
 conda activate oobleck
-conda install cmake==3.27.8
 conda install pyomo==6.6.2
 conda install glpk==5.0
 conda install ipopt==3.14.13
-# pip install -r requirements.txt
+pip install -r requirements.txt
+conda install cmake==3.27.8
 
 conda clean --yes --all
 
@@ -50,10 +50,12 @@ conda pack -n oobleck
 
 # install gcc-13
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo apt install gcc-13 g++-13 -y
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 13 --slave /usr/bin/g++ g++ /usr/bin/g++-13
-
+# install cmake
+# sudo apt install cmake
+# conda remove cmake
 # 修改头文件中的oneapi
